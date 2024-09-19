1051da4743705f81e63cd0e7739e8f64e35b6f77
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-09-19 15:00:58 +0200

    Fix issue with `cite-style` that don't need author
    - Also document better the available options for elsevier
    - and put back authoryear cite style in our template
    
    closes #27 - Citations not rendered

--- _extensions/elsevier/elsevier.lua
+++ _extensions/elsevier/elsevier.lua
@@ -52,7 +52,7 @@ local function printList(list)
   return result
 end
 
-local bibstyle = kBibStyleDefault
+local bibstyle
 
 return {
   {
@@ -157,7 +157,7 @@ return {
   },
   {
     Cite = function(cite)
-      if bibstyle == 'number' then
+      if bibstyle == 'number' or bibstyle == 'super' then
         -- If we are numbered, force citations into normal mode
         -- as the author styles don't make sense
         for i, v in ipairs(cite.citations) do
